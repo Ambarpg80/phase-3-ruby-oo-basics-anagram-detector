f16f5e430156b05a9b1ec3f980e8556a5055d94b
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-06-10 23:16:21 -0400

    commit tree c4a8fc4bc45fe536d78463dbb3bd3c8b9582bcf4

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram# Your code goes here!
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+       comparison = array.collect do |arr|
+         arr.chars.sort == @word.chars.sort ? arr : nil 
+        end
+        comparison.compact
+
+    end
+
+
+end
+
+#word.match(array)
+#map through array 
+# compare word in array to initialized word
+#   if they match, return the word from array that matched
+#   if they don't match return empty array
